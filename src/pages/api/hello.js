0baddb5e1ee3ccda6d98268b4c098dffce2b2105
somnath khadanga <somnathkhadanga@192.168.1.6>
2024-02-11 12:43:05 +0530

    firstcommit

--- src/pages/api/hello.js
+++ src/pages/api/hello.js
@@ -1,5 +1,8 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default function handler(req, res) {
+  console.log("====================================");
+  console.log("hello", req.body);
+  console.log("====================================");
   res.status(200).json({ name: "John Doe" });
 }
