91b917a748bf738a9f9e0c9dc65b220fdc6e6d53
somnath khadanga <somnathkhadanga@192.168.1.3>
2024-02-12 13:24:47 +0530

    hello

--- src/pages/api/hello.js
+++ src/pages/api/hello.js
@@ -1,10 +1,9 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import fs from "fs/promises"; // Using promises for asynchronous operations
 import * as sdk from "microsoft-cognitiveservices-speech-sdk";
-const speechRecognizer = new sdk.SpeechRecognizer(speechConfig, audioConfig);
 
-speech_key = "dbbfb9e4b6c645818edcb84fc8bbaff6";
-speech_region = "eastus";
+let speech_key = "dbbfb9e4b6c645818edcb84fc8bbaff6";
+let speech_region = "eastus";
 
 export default async function handler(req, res) {
   if (req.method === "POST") {
@@ -19,14 +18,10 @@ export default async function handler(req, res) {
       console.log("====================================");
       console.log(audioData);
       console.log("====================================");
-      function fromStream() {
+      const fromStream = () => {
         let pushStream = sdk.AudioInputStream.createPushStream();
 
-        on("data", function (arrayBuffer) {
-          pushStream.write(audioData.slice());
-        }).on("end", function () {
-          pushStream.close();
-        });
+        pushStream.write(audioData.slice());
 
         let audioConfig = sdk.AudioConfig.fromStreamInput(pushStream);
         let speechRecognizer = new sdk.SpeechRecognizer(
@@ -35,12 +30,12 @@ export default async function handler(req, res) {
         );
         speechRecognizer.recognizeOnceAsync((result) => {
           console.log(`RECOGNIZED: Text=${result.text}`);
+          res.status(200).json({ text: result.text });
           speechRecognizer.close();
         });
-      }
+      };
+      res.status(200).json({ text: "hello" });
       // fromStream();
-
-      res.status(200).json({ text: result.text });
     } catch (error) {
       console.error(error);
       res.status(500).json({ error: "Error during speech recognition" });
